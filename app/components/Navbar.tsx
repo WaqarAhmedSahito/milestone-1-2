ec585ae8ea78686b1e698763edf07d1f6df06f42
WaqarAhmedSahito <wa005423@gmail.com>
2024-12-17 13:44:11 +0500

    fix link

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -18,16 +18,28 @@ export default function Navbar() {
                             <div className="flex items-center">
                             </div>
                         </div>
+
                         <div className="hidden md:block">
+                            <div className="ml-4 flex items-center space-x-4">
+                            <Link href="/" className="bg-black text-white rounded-lg p-2 hover:bg-white hover:text-black transition">Home</Link>
+                           <Link href="/about" className="bg-black text-white rounded-lg p-2 hover:bg-white hover:text-black transition">About</Link>
+                           <Link href="/contact" className="bg-black text-white rounded-lg p-2 hover:bg-white hover:text-black transition">Contact</Link>
+                           <Link href="/education" className="bg-black text-white rounded-lg p-2 hover:bg-white hover:text-black transition">Qualification</Link>
+                           <Link href="/faq" className="bg-black text-white rounded-lg p-2 hover:bg-white hover:text-black transition">FAQ</Link>
+                            
+                            </div>
+                        </div>
+
+                        {/* <div className="hidden md:block">
                             <div className="ml-4 flex items-center space-x-4">
                             <Link href="/"> <a className="bg-black text-white rounded-lg p-2 hover:bg-white hover:text-black transition">Home</a></Link>
                            <Link href="/about"> <a className="bg-black text-white rounded-lg p-2 hover:bg-white hover:text-black transition">About</a></Link>
                            <Link href="/contact"> <a className="bg-black text-white rounded-lg p-2 hover:bg-white hover:text-black transition">Contact</a></Link>
-                           <Link href="/education"> <a className="bg-black text-white rounded-lg p-2 hover:bg-white hover:text-black transition">Qualification</a></Link>
+                           <Link href="/education" className="bg-black text-white rounded-lg p-2 hover:bg-white hover:text-black transition">Qualification</Link>
                            <Link href="/faq"> <a className="bg-black text-white rounded-lg p-2 hover:bg-white hover:text-black transition">FAQ</a></Link>
                             
                             </div>
-                        </div>
+                        </div> */}
                         <div className="md:hidden flex items-center">
                             <button
                                 className="inline-flex items-center justify-center p-2 rounded-md text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
@@ -71,11 +83,11 @@ export default function Navbar() {
                 {isClick && (
                     <div className="md:hidden">
                         <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                           <Link href="/"> <a className="bg-black text-white rounded-lg p-2 hover:bg-white hover:text-black transition">Home</a></Link>
-                           <Link href="/about"> <a className="bg-black text-white rounded-lg p-2 hover:bg-white hover:text-black transition">About</a></Link>
-                           <Link href="/contact"> <a className="bg-black text-white rounded-lg p-2 hover:bg-white hover:text-black transition">Contact</a></Link>
-                           <Link href="/education"> <a className="bg-black text-white rounded-lg p-2 hover:bg-white hover:text-black transition">Qualification</a></Link>
-                           <Link href="/faq"> <a className="bg-black text-white rounded-lg p-2 hover:bg-white hover:text-black transition">FAQ</a></Link>
+                           <Link href="/" className="bg-black text-white rounded-lg p-2 hover:bg-white hover:text-black transition">Home</Link>
+                           <Link href="/about" className="bg-black text-white rounded-lg p-2 hover:bg-white hover:text-black transition">About</Link>
+                           <Link href="/contact" className="bg-black text-white rounded-lg p-2 hover:bg-white hover:text-black transition">Contact</Link>
+                           <Link href="/education" className="bg-black text-white rounded-lg p-2 hover:bg-white hover:text-black transition">Qualification</Link>
+                           <Link href="/faq" className="bg-black text-white rounded-lg p-2 hover:bg-white hover:text-black transition">FAQ</Link>
                             
                         </div>
                     </div>
