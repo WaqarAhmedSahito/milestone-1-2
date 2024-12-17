4fbd7f78dd9438799222e0581f73573acfb91b98
WaqarAhmedSahito <wa005423@gmail.com>
2024-12-17 14:34:43 +0500

    c

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   eslint:{ignoreDuringBuilds:true,},
+  output: 'standalone',
+  distDir: 'build',
 };
 
 export default nextConfig;
